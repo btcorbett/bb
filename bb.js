2edfdb594d2584f3647109558c5e919e37f9f8e4
btcorbett <66735769+btcorbett@users.noreply.github.com>
2020-06-29 16:25:25 +0100

    Add files via upload

---
+++ bb.js
@@ -0,0 +1,8 @@
+function access() {
+  var user = document.getElementById("user-in");
+  var pass = document.getElementById("pass-in");
+  var resp = document.getElementById("resp");
+  var user_l = user.value.length;
+  var pass_l = user.value.length;
+
+}
