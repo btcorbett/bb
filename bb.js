a57f4043bec0283b8cee197edeeccef714080409
btcorbett <66735769+btcorbett@users.noreply.github.com>
2020-06-30 18:12:19 +0100

    Add files via upload

--- bb.js
+++ bb.js
@@ -1,3 +1,7 @@
+const groupArr = ["Greetings", "British", "Compliments", "Love", "Northern"];
+
+document.getElementById("groupTxt").innerHTML = groupArr[0];
+
 function access() {
   /*
   var user = document.getElementById("user-in");
@@ -14,4 +18,47 @@ function iconPlay(id) {
   var aud = new Audio("audio/" + id + ".mp3");
 
   aud.play();
+}
+
+function groupSlide(id) {
+  var arrow = document.getElementById(id);
+  var gTxt = document.getElementById("groupTxt");
+  var txt = gTxt.innerHTML, pos = groupArr.indexOf(txt), entries = groupArr.length - 1;
+
+  switch (id) {
+    case "arrowL":
+      switch (true) {
+        case pos == 0:
+          gTxt.classList.add("slide-l");
+          window.setTimeout(function(){
+            gTxt.innerHTML = "";
+            gTxt.classList.remove("slide-l");
+          }, 75);
+          window.setTimeout( function() {
+            gTxt.classList.add("slide-r");
+            window.setTimeout(function(){
+              gTxt.classList.remove("slide-r");
+              gTxt.innerHTML = groupArr[entries];
+            }, 75);
+          }, 150);
+          break;
+        case pos > 0:
+        case pos <= entries:
+          gTxt.innerHTML = groupArr[pos - 1];
+          break;
+      }
+      break;
+    case "arrowR":
+      switch (true) {
+        case pos == entries:
+          gTxt.innerHTML = groupArr[0];
+          break;
+        case pos < entries:
+        case pos >= 0:
+          gTxt.innerHTML = groupArr[pos + 1];
+          break;
+      }
+      break;
+  }
+
 }
